c8903519daf9f3240b775eb2c800809028a77fca
Cam Ashton <cam@msp360.com>
2015-07-24 00:46:23 +0100

    Update boxstarter_chocolatey_standardpc.ps1
    comment out winrm

--- boxstarter_chocolatey_standardpc.ps1
+++ boxstarter_chocolatey_standardpc.ps1
@@ -44,12 +44,12 @@ cinst vcredist2010 -y #--acceptlicense
 cinst dotnet4.5 -y #--acceptlicense
 
 # --  WINRM Setup --
-winrm quickconfig -q
-winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
-winrm set winrm/config @{MaxTimeoutms="1800000"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/service/auth @{Basic="true"}
-sc config WinRM start= auto
+#winrm quickconfig -q
+#winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
+#winrm set winrm/config @{MaxTimeoutms="1800000"}
+#winrm set winrm/config/service @{AllowUnencrypted="true"}
+#winrm set winrm/config/service/auth @{Basic="true"}
+#sc config WinRM start= auto
 
 cinst javaruntime -y #--acceptlicense
 
